09e15aa680953005aa0c288d4f232ee749a352d8
donrogo <rogina@gmx.at>
2015-05-21 21:50:46 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this is an function using the <<- operator which is used to
+## assign an value to an object, but not in the same environment
+## but a different one.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix sets and gets the value of the matrix and
+##  then it sets and gets the inverse of it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #create cache and initialize to NULL
+  i <- NULL
+  
+   #create matrix in wd  
+   set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the matrix is not empty. If it is not
+## empty then it return the cached date. If it is empty it
+## computes the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
